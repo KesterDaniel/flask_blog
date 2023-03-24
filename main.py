9b948a318979e147c416bf7f966160b37312193f
KesterDaniel <kesterdan17@gmail.com>
2023-03-24 11:25:12 +0100

    rendering blog post in read blog page

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ def home():
         blog_data = json.load(blog_file)
     return render_template("index.html", blog_posts=blog_data)
 
-@app.route("/readblog/<id>")
-def read_blog(id):
+@app.route("/readblog/<int:b_id>")
+def read_blog(b_id):
     with open("blog-data.json") as blog_file:
-        blog_data = json.load(blog_file)
-    return render_template("post.html", blog_posts=blog_data, blog_id=id)
+        all_blogs = json.load(blog_file)
+
+    for blog in all_blogs:
+        if blog["id"] == b_id:
+            blog_data = blog
+
+    return render_template("post.html", blog_post=blog_data, blog_id=b_id)
 
 
 if __name__ == "__main__":
