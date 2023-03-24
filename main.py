cb6c5bfc89988529898dfef1879dd3650328cce4
KesterDaniel <kesterdan17@gmail.com>
2023-03-24 10:52:32 +0100

    rendering all blogs on the page

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     with open("blog-data.json") as blog_file:
         blog_data = json.load(blog_file)
-    return render_template("index.html", blog_post=blog_data)
+    return render_template("index.html", blog_posts=blog_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
