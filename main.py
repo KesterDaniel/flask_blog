c93056c28953e42f1d95411e131a5b7e5fc814a9
KesterDaniel <kesterdan17@gmail.com>
2023-03-24 11:07:17 +0100

    building the read blog route

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ def home():
         blog_data = json.load(blog_file)
     return render_template("index.html", blog_posts=blog_data)
 
+@app.route("/readblog/<id>")
+def read_blog(id):
+    with open("blog-data.json") as blog_file:
+        blog_data = json.load(blog_file)
+    return render_template("post.html", blog_posts=blog_data, blog_id=id)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
